b6bee12f169e1072a747a4608b91a9a33df742e8
Michael <heverlym@gmail.com>
2019-06-20 17:42:52 -0400

    fist commit

---
+++ apps/time_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
+
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, 'time_app/index.html', context)
+
